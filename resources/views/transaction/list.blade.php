65c0b80f42eb36dbdc883afd6c8170a4bbb79dde
alwdev <69068330+alwdev@users.noreply.github.com>
2024-12-17 12:54:17 +0700

    text

--- resources/views/transaction/list.blade.php
+++ resources/views/transaction/list.blade.php
@@ -369,8 +369,7 @@
         function approveDeposit(form){
             Swal.fire({
                     title: 'แจ้งเตือน',
-                    text: "ต้องการอัพเดตสถานะหรือไม่?",
-                    text: "***คำเตือนหากเป็นการ ถอนเงิน Admin ต้องทำรายการโอนเงินเองที่แอปธนาคาร***",
+                    text: "ต้องการอัพเดตสถานะหรือไม่?  ***คำเตือนหากเป็นการ ถอนเงิน Admin ต้องทำรายการโอนเงินเองที่แอปธนาคาร***",
                     type: 'warning',
                     showCancelButton: true,
                     confirmButtonText: 'ใช่',
