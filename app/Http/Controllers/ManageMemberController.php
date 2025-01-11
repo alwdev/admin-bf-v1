f0f6c6ed656b6f415f7bf99600fc140328826a19
alwdev <69068330+alwdev@users.noreply.github.com>
2025-01-11 15:16:40 +0700

    deposit

--- app/Http/Controllers/ManageMemberController.php
+++ app/Http/Controllers/ManageMemberController.php
@@ -69,7 +69,7 @@ class ManageMemberController extends Controller
                 $amount_betflix = 0;
                 if($transfer->promotion_id != 0){
                     $pro = Promotion::find($transfer->promotion_id);
-                    $user_transfer = Transfer::where('member_id',$member->id)->where('status',2)->get();  /// เช็คฝากครั้งแรก
+                    $user_transfer = Transfer::where('member_id',$member->id)->where('status',2)->where('type','deposit')->get();  /// เช็คฝากครั้งแรก
                     $user_transfer_count = $user_transfer->count();
 
                     if($user_transfer_count == 0){
