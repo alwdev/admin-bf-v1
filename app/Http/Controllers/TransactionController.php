e9c18724050af1e7156858dfffec92e9129708a1
alwdev <69068330+alwdev@users.noreply.github.com>
2025-01-12 19:41:20 +0700

    log

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -124,12 +124,12 @@ class TransactionController extends Controller
 
 
         } catch(\Exception $e){
-            // Log::error("Error : ".$e->getMessage());
+            Log::error("Error : ".$e->getMessage());
 
-            // TelegramMessage::create()->to(env('TELEGRAM_G_ID'))
-            //     ->line('BOT '.env('APP_NAME'))
-            //     ->line('พบข้อผิดพลาดในการตรวจสอบ SMS')
-            //     ->send();
+            TelegramMessage::create()->to(env('TELEGRAM_G_ID'))
+                ->line('BOT '.env('APP_NAME'))
+                ->line('พบข้อผิดพลาดในการตรวจสอบ SMS')
+                ->send();
 
             return response()->json(['message' => 'พบข้อผิดพลาดในการตรวจสอบ SMS'], 400);
         }
