f1cc02674c2d704a0b8a250b340c6814b5462500
alwdev <69068330+alwdev@users.noreply.github.com>
2025-01-14 13:50:56 +0700

    lastTransactionHistory

--- app/Http/Controllers/TMN_Controller.php
+++ app/Http/Controllers/TMN_Controller.php
@@ -46,7 +46,7 @@ class TMN_Controller extends Controller
         $TMNOne->loginWithPin6($this->pin);
         $transfer = $TMNOne->transferP2P($request->mobile_number,$request->amount,"");
         $ransactionHistory = $TMNOne->fetchTransactionHistory(date('Y-m-d',time()-86400), date('Y-m-d',time()+86400));
-        return response()->json($transfer,$ransactionHistory[0]);
+        return response()->json([$transfer,$ransactionHistory[0]],200);
     }
 
     public function lastTransactionHistory(){
@@ -54,7 +54,7 @@ class TMN_Controller extends Controller
         $TMNOne->setData($this->tmn_key_id, $this->mobile_number, $this->login_token, $this->tmn_id);
         $TMNOne->loginWithPin6($this->pin);
         $history = $TMNOne->fetchTransactionHistory(date('Y-m-d',time()-86400), date('Y-m-d',time()+86400));
-        return response()->json($history,$history[0]);
+        return $history[0];
     }
     public function fetchTransactionHistory(){
         $TMNOne = new TMNOne();
