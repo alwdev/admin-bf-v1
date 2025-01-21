bb31ec9cf1a6a89fa5c1e4d30ab78389f2c2eb57
alwdev <69068330+alwdev@users.noreply.github.com>
2025-01-21 11:58:04 +0700

    pg_get_spin_summaryby_user

--- app/Http/Controllers/PgHardController.php
+++ app/Http/Controllers/PgHardController.php
@@ -9,41 +9,39 @@ use Illuminate\Support\Facades\Log;
 class PgHardController extends Controller
 {
     public function pg_get_spin_summaryby_user($username,$date_start,$date_end){
-        $headers = array();
-		$headers[] = 'Content-Type: application/json';
-        $headers[] = 'Authorization: '.env('PGHARD_SECRET_KEY');
-        $headers[] = 'accept: application/json';
 
-        // $startDate = Carbon::today()->format('Y-m-d').'T00:00:00Z';
-        // $endDate = Carbon::today()->format('Y-m-d').'T23:59:59Z';
-
-        $startDate=date('Y-m-d',strtotime($date_start.' day'));
-        $endDate=date('Y-m-d',strtotime($date_end.' day'));
+        $startDate=date('Y-m-d',strtotime($date_start.' day')).'T00:00:00Z';
+        $endDate=date('Y-m-d',strtotime($date_end.' day')).'T23:59:59Z';
 
 
-        error_log($username);
-        error_log($startDate);
-        error_log($endDate);
+        // error_log($username);
+        // error_log($startDate);
+        // error_log($endDate);
 
         $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, 'https://api.pghard.com/external/get-spin-order-by-username');
-		curl_setopt($curl, CURLOPT_POSTFIELDS, '{
-            "skip": 0,
-            "take": 100,
-            "startDate": "'.$startDate.'",
-            "endDate": "'.$endDate.'",
-            "username": "'.$username.'",
-            "operatorId": "'.env('PGHARD_OPERATOR_ID').'",
-            }');
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-		curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+        $payload = [
+            "skip" => 0,
+            "take" => 10,
+            "startDate" => $startDate,
+            "endDate" => $endDate,
+            "username" => $username,
+            "operatorId" => env('PGHARD_OPERATOR_ID')
+        ];
+
+        curl_setopt_array($curl, [
+            CURLOPT_URL => "https://api.pghard.com/external/get-spin-order-summary-by-usernames",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_POST => true,
+            CURLOPT_POSTFIELDS => json_encode($payload),
+            CURLOPT_HTTPHEADER => [
+                "Authorization: ".env('PGHARD_SECRET_KEY'),
+                "Content-Type: application/json"
+            ]
+        ]);
 
         $response = curl_exec($curl);
+        $data = json_decode($response, true);
         curl_close($curl);
-        return json_decode($response);
+        return $data;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -175,4 +175,4 @@ Route::get('/regenmember_idxxx', [App\Http\Controllers\SettingController::class,
 
 Route::get('/tmn_balance', [App\Http\Controllers\TMN_Controller::class, 'index']);
 
-Route::get('/pg_get_spin_summaryby_user', [App\Http\Controllers\PgHardController::class, 'pg_get_spin_summaryby_user']);
+Route::get('/pg_get_spin_summaryby_user/{username}/{start_day}/{end_day}', [App\Http\Controllers\PgHardController::class, 'pg_get_spin_summaryby_user']);
