f65d670051c9d62adcfd2fb6613220de76aacb57
alwdev <69068330+alwdev@users.noreply.github.com>
2025-01-22 23:59:26 +0700

    Update SettingController.php

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -359,7 +359,7 @@ class SettingController extends Controller
         $data->ticket_condition = $request->ticket_condition;
         $data->limit_per_day = (isset($request->limit_per_day) ? $request->limit_per_da : 0);
         $data->limit_person = (isset($request->limit_person) ? $request->limit_person : 0);
-        $data->enable = (isset($request->is_enable_cashback) ? 1 : 0);
+        $data->enable = (isset($request->enable) ? 1 : 0);
         $data->limit_withdraw = $request->limit_withdraw;
         $data->trunover = (isset($request->trunover) ? $request->trunover : 0);
         $data->win_1_reward = $request->win_1_reward;
