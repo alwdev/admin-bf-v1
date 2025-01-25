924a028ff2ebc46108da9d817b5343b9cf9c8c1b
alwdev <69068330+alwdev@users.noreply.github.com>
2025-01-25 15:11:30 +0700

    member->wallet_balance

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -33,7 +33,7 @@ class TransactionController extends Controller
             $join->on('members.id','=','transfer.member_id');
         })
         ->select(\DB::raw('transfer.*,members.bank_number,members.account_name,members.bank_name,members.username'))
-        ->where('transfer.type','!=','cashback')->where('transfer.type','!=','commission')
+        ->where('transfer.type','!=','cashback')->where('transfer.type','!=','commission')->where('transfer.type','!=','wheel')
         ->orderby('transfer.created_at','desc')
         ->get();
 
--- resources/views/manage-member/index.blade.php
+++ resources/views/manage-member/index.blade.php
@@ -77,7 +77,7 @@
                             @endif
                             <td>{{ $member->fullname }}</td>
                             @php
-                                $member_balance = app(\App\Http\Controllers\BetflixController::class)->Balance($member->username);
+                                $member_balance = $member->wallet_balance; //app(\App\Http\Controllers\BetflixController::class)->Balance($member->username);
                             @endphp
                             <td class="text-right">{{ $member_balance }} ฿
                                 @if( json_decode(auth()->user()->permissions)->manageuser > 2  )
