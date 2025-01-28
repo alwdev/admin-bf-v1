28175ef3d0ce1278a1bdcfd4f846b63e04be4fd3
alwdev <69068330+alwdev@users.noreply.github.com>
2025-01-28 20:43:12 +0700

    Update index.blade.php

--- resources/views/manage-member/index.blade.php
+++ resources/views/manage-member/index.blade.php
@@ -202,7 +202,7 @@
                                     <div class="modal-dialog  modal-dialog-centered modal-dialog-scrollable">
                                       <div class="modal-content">
                                         <div class="modal-header">
-                                          <h5 class="modal-title" id="updateBankAccount{{ $key }}Label">แก้ไขยอดเงิน</h5>
+                                          <h5 class="modal-title" id="updateBankAccount{{ $key }}Label">แก้ไขบัญชีธนาคาร</h5>
                                           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                             <span aria-hidden="true">&times;</span>
                                           </button>
