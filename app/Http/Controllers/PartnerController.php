52b0d37dc935cfb0995495753294c20cc58013ae
alwdev <69068330+alwdev@users.noreply.github.com>
2025-01-29 21:53:49 +0700

    partner

---
+++ app/Http/Controllers/PartnerController.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Partner;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Log;
+use App\Models\Logs;
+use App\Models\PromotionUsed;
+use App\Models\Affiliate;
+use App\Models\WheelSpin;
+use App\Models\Setting;
+use NotificationChannels\Telegram\TelegramMessage;
+
+
+class PartnerController extends Controller
+{
+    //
+    public function index(){
+        $list = Partner::all();
+        // foreach ($list as $key => $value) {
+        //     # code...
+        //     $transfer = Transfer::whereIn()
+        // }
+
+        return view('partner.index',compact('list'));
+    }
+
+    public function add(){
+        $code = $this->RandomString(10);
+        return view('partner.add',compact('code'));
+    }
+
+    public function edit($id){
+        $data = Partner::find($id);
+        return view('partner.edit',compact('data'));
+    }
+
+    public function create(Request $request){
+        
+        $request->validate([
+            'contact_name' => ['required','string','max:255'],
+            'slug_name' => ['required','string','unique:partner'],
+            'rate' => ['required'],
+        ]);
+
+        $partner = new Partner;
+        $partner->slug_name = $request->slug_name;
+        $partner->url = env('APP_FRONT_URL').'/register?partner='.$request->slug_name;
+        $partner->contact_name = $request->contact_name;
+        $partner->contact_phonenumber = $request->contact_phonenumber;
+        $partner->contact_email = $request->contact_email;
+        $partner->rate = $request->rate;
+        $partner->save();
+
+        return redirect()->route('partner.index')->with('status','Partner added successfully');
+    }
+
+    public function update(Request $request){
+        
+        $request->validate([
+            'id' => ['required'],
+            'contact_name' => ['required','string','max:255'],
+            'slug_name' => ['required','string',Rule::unique('partner')->ignore($request->id)],
+            'rate' => ['required'],
+        ]);
+
+        $partner = Partner::find($request->id);
+        $partner->slug_name = $request->slug_name;
+        $partner->url = env('APP_FRONT_URL').'/register?partner='.$request->slug_name;
+        $partner->contact_name = $request->contact_name;
+        $partner->contact_phonenumber = $request->contact_phonenumber;
+        $partner->contact_email = $request->contact_email;
+        $partner->rate = $request->rate;
+        $partner->save();
+
+        return redirect()->route('partner.index')->with('status','Partner added successfully');
+    }
+
+    function RandomString($length) {
+        $original_string = array_merge(range(0,9), range('a','z'), range('A', 'Z'));
+        $original_string = implode("", $original_string);
+        return substr(str_shuffle($original_string), 0, $length);
+    }
+
+    function partner_call_winlose(){
+        set_time_limit(3000000000);
+        Log::info("Run Check");
+        TelegramMessage::create()->to(env('TELEGRAM_G_ID'))
+        ->line(env('APP_NAME'))
+        ->line('BOT เริ่มทำการ ส่วนแบ่ง Partner')
+        ->send();
+
+
+
+        $partners = Partner::all();
+        Log::info("Total Partner  : ".count($partners));
+        foreach ($partners as $value) {
+            sleep(2);
+            $total_commission = 0;
+            Log::info("Member main : " . $value->contanct_name.'uder partner count = '.count(json_decode($value->members)));
+            if(json_decode($value->ref_user)){
+                set_time_limit(3000000000);
+                foreach(json_decode($value->members) as $_member){
+                    sleep(3);
+
+                    $under_member = Members::where('id',$_member)->first();
+                    Log::info("Under of ".$value->contanct_name." member : " .$under_member->username);
+
+                    try{
+                        $bf_total_bet = app(\App\Http\Controllers\BetflixController::class)->Single_Member_Report_all_Provider($under_member->username,-1,-1);
+                        if($bf_total_bet){
+                            $total_bet = $bf_total_bet->valid_amount;
+                            $winlose = $bf_total_bet->winloss;
+                            Log::info("bf_total_bet : " . $bf_total_bet->valid_amount);
+                        }else{
+                            Log::info("bf_total_bet : " . $bf_total_bet->msg);
+                        }
+                    } catch (\Exception $e) {
+                        Log::info('Betflix API Error : '.$e->getMessage());
+                        $total_bet =0;
+                        $winlose =0;
+                        continue;
+                    }
+
+                    try{
+                        $pg_total_bet = app(\App\Http\Controllers\PgHardController::class)->pg_get_spin_summaryby_user($under_member->username,-1,-1);
+
+                        if(count($pg_total_bet['data']) > 0){
+                            Log::info("pg_total_bet : " . $pg_total_bet['data'][0]['totalAmount']);
+                            $total_bet = $total_bet + $pg_total_bet['data'][0]['totalAmount'];
+                        }else{
+                            Log::info('PgHard API No have User Data '.$under_member->username);
+                        }
+                    } catch (\Exception $e) {
+                        Log::info('PgHard API Error : '.$e->getMessage());
+                        $pg_total_bet =0;
+                    }
+
+                    Log::info("total_bet : ".$total_bet);
+
+                    if($total_bet > 1){
+
+                        $commission = abs($winlose) * ($value->rate/ 100);
+                        $total_commission += $commission;
+                        Log::info("commission ยอด winlose : ".$winlose." commission : ".$commission);
+
+                    }
+                }
+                if($total_commission > 0){
+                    $value->total_profit = $total_commission;
+                    $value->save();
+                }
+
+                // $bf_deposit=  app(\App\Http\Controllers\BetflixController::class)->Master_Deposit($value->username,floor($commission));
+                // Log::info('Deposit commission to Betflix  '.$bf_deposit.' '.floor($commission).' User =  '.$value->username);
+                // $value->wallet_balance = (float) ($value->wallet_balance + $commission);
+                // $value->save();
+            }
+
+        }
+        Log::info('success Run ส่วนแบ่ง Partner');
+        TelegramMessage::create()->to(env('TELEGRAM_G_ID'))
+            ->line(env('APP_NAME'))
+            ->line('BOT สิ้นสุดการ Run ส่วนแบ่ง Partner ')
+            ->send();
+        return 'success';
+    }
+
+}
---
+++ resources/views/partner/index.blade.php
@@ -0,0 +1,317 @@
+@extends('layouts.guest')
+@section('styles')
+{{-- <link href="{{ asset('plugins/datatables/dataTables.bootstrap4.css')}}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('plugins/datatables/responsive.bootstrap4.css')}}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('plugins/datatables/buttons.bootstrap4.css')}}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('plugins/datatables/select.bootstrap4.css')}}" rel="stylesheet" type="text/css" /> --}}
+@endsection
+@section('content')
+   <!-- start page title -->
+   <div class="row">
+    <div class="col-12">
+        <div class="page-title-box d-flex align-items-center justify-content-between">
+            <h4 class="mb-0 font-size-18">จัดการพันธมิตร</h4>
+
+            <div class="page-title-right">
+                <ol class="breadcrumb m-0">
+                    <li class="breadcrumb-item"><a href="javascript: void(0);">เพจ</a></li>
+                    <li class="breadcrumb-item active">จัดการพันธมิตร</li>
+                </ol>
+            </div>
+            
+        </div>
+    </div>
+</div>     
+<!-- end page title -->
+
+        <div class="row">
+            <div class="card-header text-right" style="background: transparent;">
+              
+            </div>
+            <div class="col-12 card">
+           
+                    <div class="card-body">
+                        @if( json_decode(auth()->user()->permissions)->manageuser > 2  )
+                        <a type="button" class="btn btn-primary btn-gold waves-effect waves-light" href="{{ route('partner.add') }}">เพิ่มพันธมิตร</a>
+                        @endif
+                        <h4 class="card-title"></h4>
+                        <p class="card-subtitle mb-4">
+                        </p>
+        
+                        <table id="basic-datatable" class="table m-10 table-bordered"
+                        data-filter-control="true"
+                        data-toggle="table"
+                        data-search="true"
+                        data-show-export="false"
+                        data-click-to-select="false"
+                        data-pagination="true"
+                        data-url="">
+                        <thead  class="table-light">
+                                <tr>
+                                    <th data-field="contact_name" data-sortable="true">ชื่อพันธมิตร</th>
+                                    <th data-field="contact_phonenumber"  data-sortable="true">เบอร์โทรศัพท์</th>
+                                    <th data-field="slug_name"  data-sortable="true">รหัส</th>
+                                    <th data-field="url"  data-sortable="true">URL</th>
+                                    <th data-sortable="true">created_at</th>
+                                    <th></th>
+                                </tr>
+                            </thead>  
+                            <tbody>
+                                @foreach ($list as $partner)                            
+                                <tr>
+                                    <td>{{ $partner->contact_name }}</td>
+                                    <td>{{ $partner->contact_mail }}</td>
+                                    <td>{{ $partner->slug_name }}</td>
+                                    <td>{{ $partner->url }}</td>
+                                    <td>
+                                        {{ $partner->created_at->format('d/m/Y H:i:s') }}
+                                    </td>
+                                    <td>
+                                        <a href="{{ route('partner.edit',$partner->id) }}" type="button" class="btn btn-secondary btn-sm waves-effect waves-light"><i class="bx bx-edit-alt"></i> </a>
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+        
+                    </div> <!-- end card body-->
+
+            </div><!-- end col-->
+        </div>
+
+
+<!-- end row-->
+@endsection
+@section('scripts')
+
+    <!-- third party js -->
+    {{-- <script src="{{ asset('plugins/datatables/jquery.dataTables.min.js')}}"></script>
+    <script src="{{ asset('plugins/datatables/dataTables.bootstrap4.js')}}"></script>
+    <script src="{{ asset('plugins/datatables/dataTables.responsive.min.js')}}"></script>
+    <script src="{{ asset('plugins/datatables/responsive.bootstrap4.min.js')}}"></script>
+    <script src="{{ asset('plugins/datatables/dataTables.buttons.min.js')}}"></script>
+    <script src="{{ asset('plugins/datatables/buttons.bootstrap4.min.js')}}"></script>
+    <script src="{{ asset('plugins/datatables/buttons.html5.min.js')}}"></script>
+    <script src="{{ asset('plugins/datatables/buttons.flash.min.js')}}"></script>
+    <script src="{{ asset('plugins/datatables/buttons.print.min.js')}}"></script>
+    <script src="{{ asset('plugins/datatables/dataTables.keyTable.min.js')}}"></script>
+    <script src="{{ asset('plugins/datatables/dataTables.select.min.js')}}"></script>
+    <script src="{{ asset('plugins/datatables/pdfmake.min.js')}}"></script>
+    <script src="{{ asset('plugins/datatables/vfs_fonts.js')}}"></script> --}}
+    <!-- third party js ends -->
+
+    <!-- Datatables init -->
+    {{-- <script src="{{ asset('pages/datatables-demo.js')}}"></script> --}}
+    <link rel="stylesheet" href="{{ asset('plugins/datatables/dataTables.bootstrap4.css') }}">
+    <link rel="stylesheet" href="{{ asset('plugins/datatables/responsive.bootstrap4.css') }}">
+    <link rel="stylesheet" href="{{ asset('plugins/datatables/buttons.bootstrap4.css') }}">
+    <link href="https://unpkg.com/bootstrap-table@1.21.2/dist/bootstrap-table.min.css" rel="stylesheet">
+
+    <script src="{{ asset('plugins/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('plugins/datatables/dataTables.bootstrap4.js') }}"></script>
+    <script src="{{ asset('plugins/datatables/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ asset('plugins/datatables/responsive.bootstrap4.min.js') }}"></script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <script src="https://unpkg.com/bootstrap-table@1.21.2/dist/bootstrap-table.min.js"></script>
+    <script src="https://unpkg.com/bootstrap-table@1.21.2/dist/extensions/filter-control/bootstrap-table-filter-control.min.js"></script>
+
+    <script>
+    @if (session('status'))
+
+        Swal.fire({
+            position: 'top-end',
+            type: 'success',
+            title: 'Your work has been saved',
+            showConfirmButton: false,
+            timer: 1500
+        })
+
+    @endif
+    @if (session('del_status'))
+
+        Swal.fire({
+            position: 'top-end',
+            type: 'success',
+            title: 'Your work has been saved',
+            showConfirmButton: false,
+            timer: 1500
+        })
+
+    @endif
+
+    function confirmPass(userid) {
+        Swal.mixin({
+        input: 'text',
+        confirmButtonText: 'ยืนยัน &rarr;',
+        showCancelButton: true,
+        cancelButtonText: 'ยกเลิก',
+        progressSteps: ['1', '2']
+      }).queue([
+        {
+          title: 'แจ้งเตือน',
+          text: 'กรุณายืนยันรหัสผ่านของคุณเพือดำเนินการต่อ.'
+        }
+      ]).then( function (result) {
+
+        if (result.value) {
+            if (result.value != ""){
+
+                $.ajax({
+				type: 'post',
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                },
+				url: '{{ route('manageuser.checkPassword') }}',
+				data: { password:result.value,userId:userid },
+				success: function (data) {
+					if(data!=false){
+                        Swal.fire(
+                        {
+                            title: '',
+                            html: '<h4>ชื่อผู้ใช้: '+data.name+'</br></h4>' +
+                            '<h4>รหัสผ่าน: '+data.truepass+'</br></h4>',
+                            type: 'success',
+                            confirmButtonText: 'ตกลง',
+                            confirmButtonClass: 'btn btn-confirm mt-2'
+                        }
+                    )
+                    }else{
+                        Swal.fire({
+                        type: 'error',
+                        title: "แจ้งเตือน!",
+                        text: "รหัสผ่านไม่ถูกต้อง",
+                        });
+                    }
+				}
+			});
+
+  
+            }
+        }
+      }) 
+    }
+    function changePass(userid) {
+        Swal.mixin({
+            input: 'text',
+            confirmButtonText: 'ยืนยัน &rarr;',
+            showCancelButton: true,
+            cancelButtonText: 'ยกเลิก',
+            progressSteps: ['1', '2']
+        }).queue([
+            {
+            title: 'แจ้งเตือน',
+            text: 'กรุณายืนยันรหัสผ่านของคุณเพือดำเนินการต่อ.'
+            }
+        ]).then( function (result) {
+            if (result.value) {
+                if (result.value != ""){
+
+                    $.ajax({
+                        type: 'post',
+                        headers: {
+                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                        },
+                        url: '{{ route('manageuser.checkPassword') }}',
+                        data: { password:result.value,userId:userid },
+                        success: function (data) {
+                            if(data!=false){
+                                Swal.mixin({
+                                    input: 'text',
+                                    confirmButtonText: 'ยืนยัน &rarr;',
+                                    showCancelButton: true,
+                                    cancelButtonText: 'ยกเลิก',
+                                    progressSteps: ['1', '2']
+                                }).queue([
+                                    {
+                                    title: 'แจ้งเตือน',
+                                    text: 'รหัสผ่านใหม่'
+                                    }
+                                ]).then( function (result2) {
+                                    $.ajax({
+                                        type: 'post',
+                                        headers: {
+                                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                                        },
+                                        url: '{{ route('manageuser.changePassword') }}',
+                                        data: { password:result.value,userId:userid },
+                                        success: function (data) {
+                                            if(data!=false){
+                                                Swal.fire(
+                                                    {
+                                                        title: 'เปลี่ยนรหัสผ่านสำเร็จ',
+                                                        html: '<h4>ชื่อผู้ใช้: '+data.name+'</br></h4>' +
+                                                        '<h4>รหัสผ่าน: '+data.truepass+'</br></h4>',
+                                                        type: 'success',
+                                                        confirmButtonText: 'ตกลง',
+                                                        confirmButtonClass: 'btn btn-confirm mt-2'
+                                                    }
+                                                )
+                                            }else{
+                                                // Swal.fire({
+                                                //     type: 'error',
+                                                //     title: "แจ้งเตือน!",
+                                                //     text: "รหัสผ่านไม่ถูกต้อง",
+                                                // });
+                                            }
+                                        }
+                                    });
+                                })
+                            }else{
+                                Swal.fire({
+                                type: 'error',
+                                title: "แจ้งเตือน!",
+                                text: "รหัสผ่านไม่ถูกต้อง",
+                                });
+                            }
+                        }
+                    });
+
+                }
+            }
+      }) 
+    }
+
+    function deluser(userid) {
+        Swal.mixin({
+        input: 'text',
+        confirmButtonText: 'ยืนยัน &rarr;',
+        showCancelButton: true,
+        cancelButtonText: 'ยกเลิก',
+        progressSteps: ['1', '2']
+      }).queue([
+        {
+          title: 'แจ้งเตือน',
+          text: 'กรุณายืนยันรหัสผ่านของคุณเพือดำเนินการต่อ.'
+        }
+      ]).then( function (result) {
+
+        if (result.value) {
+            if (result.value != ""){
+                $.ajax({
+				type: 'post',
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                },
+				url: '{{ route('manageuser.checkPassword') }}',
+				data: { password:result.value,userId:userid },
+				success: function (data) {
+					if(data!=false){
+                        $('#del'+userid).submit();
+                    }else{
+                        Swal.fire({
+                        type: 'error',
+                        title: "แจ้งเตือน!",
+                        text: "รหัสผ่านไม่ถูกต้อง",
+                        });
+                    }
+				}
+			});
+                
+                      
+            }
+        }
+      }) 
+    }
+    </script>
+@endsection
