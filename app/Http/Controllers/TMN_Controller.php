ee03be1ea1686a2934fddbda3923af5ce8853d7d
alwdev <69068330+alwdev@users.noreply.github.com>
2025-01-29 16:57:38 +0700

    env TMN

--- app/Http/Controllers/TMN_Controller.php
+++ app/Http/Controllers/TMN_Controller.php
@@ -109,8 +109,8 @@ class TMN_Controller extends Controller
 
     public function lastTransactionHistory(){
         $TMNOne = new TMNOne();
-        $TMNOne->setData($this->tmn_key_id, $this->mobile_number, $this->login_token, $this->tmn_id);
-        $TMNOne->loginWithPin6($this->pin);
+        $TMNOne->setData(env('TMN_KEY_ID'), env('TMN_MOBILE_NUMBER'), env('TMN_TOKEN'), env('TMN_ID'));
+        $TMNOne->loginWithPin6(env('TMN_PIN'));
         $history = $TMNOne->fetchTransactionHistory(date('Y-m-d',time()-86400), date('Y-m-d',time()+86400));
         return $history[0];
     }
