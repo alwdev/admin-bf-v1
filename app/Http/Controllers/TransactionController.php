7cedc02e1c947be7e91472a84f10c057d4826cd7
alwdev <69068330+alwdev@users.noreply.github.com>
2025-02-13 21:30:19 +0700

    //sms

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -347,6 +347,10 @@ class TransactionController extends Controller
                 ->line('ทำรายการสำเร็จ โอนเครดิตเข้า '.$member->username)
                 ->line('จำนวน :'.$amount)
                 ->line('Bonus :'.$bonus)
+                // ->button('View page', env('APP_URL'))
+                // ->button('View page',env('APP_URL'))
+                // ->keyboard('Button 1')
+                // ->keyboard('Button 2')
                 ->send();
 
                 return response()->json(['message' => 'SMS request sent successfully.','txt' => 'Amount :'.$amount], 200);
@@ -354,9 +358,13 @@ class TransactionController extends Controller
                 TelegramMessage::create()->to(env('TELEGRAM_G_ID'))
                 // ->content('Choose an option:')
                 ->line('BOT '.env('APP_NAME'))
-                ->line('sms_step2 ไม่พบรายการโอนเงินในช่วงเวลา ')
+                ->line('ไม่พบรายการโอนเงินในช่วงเวลา ')
                 ->line('จำนวน :'.$amount)
                 ->line('subMinute(5)'.now()->subMinute(5))
+                // ->button('View page', env('APP_URL'))
+                // ->button('View page',env('APP_URL'))
+                // ->keyboard('Button 1')
+                // ->keyboard('Button 2')
                 ->send();
                 return response()->json(['message' => 'ไม่พบรายการโอนเงินในช่วงเวลา','txt' => 'Amount :'.$amount], 404);
             }
