ca4e18a856881562ca1187b3ecab9b33ce947efd
alwdev <69068330+alwdev@users.noreply.github.com>
2025-02-17 17:38:19 +0700

    -7 date

--- app/Http/Controllers/PartnerController.php
+++ app/Http/Controllers/PartnerController.php
@@ -117,7 +117,7 @@ class PartnerController extends Controller
                     Log::info("Under of ".$value->contanct_name." member : " .$under_member->username);
 
                     try{
-                        $bf_total_bet = app(\App\Http\Controllers\BetflixController::class)->Single_Member_Report_all_Provider($under_member->username,-1,-1);
+                        $bf_total_bet = app(\App\Http\Controllers\BetflixController::class)->Single_Member_Report_all_Provider($under_member->username,-7,-1);
                         if($bf_total_bet){
                             $total_bet = $bf_total_bet->valid_amount;
                             $winlose = $bf_total_bet->winloss;
@@ -160,7 +160,7 @@ class PartnerController extends Controller
                     $value->total_profit = $value->total_profit + $total_commission;
                     $value->save();
 
-                    $start_date=date('Y-m-d',strtotime('-1 day'));
+                    $start_date=date('Y-m-d',strtotime('-7 day'));
                     $end_date=date('Y-m-d',strtotime('-1 day'));
                     PartnerCommission::create([
                         'partner_id' => $value->id,
