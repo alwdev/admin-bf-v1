23bea09b47a8c8d71ad97d696d18205e86ad05e5
alwdev <69068330+alwdev@users.noreply.github.com>
2025-06-14 17:05:29 +0700

    logo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -43,7 +43,7 @@
                                         <div class="card-body">
                                             <div class="text-center mb-4 mt-3">
                                                 <a href="/">
-                                                    <span><img src="{{ asset('logo3.png') }}" alt="" height="50"></span>
+                                                    <span><img src="{{ asset('logo3.png') }}" alt="" height="120"></span>
                                                 </a>
                                             </div>
                                             <form action="{{ route('login') }}" method="post" class="p-2">
