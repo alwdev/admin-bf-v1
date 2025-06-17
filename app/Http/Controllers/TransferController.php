57016c42354796f2d544c7bbfd4326d2615f5618
alwdev <69068330+alwdev@users.noreply.github.com>
2025-06-17 18:24:16 +0700

    Update TransferController.php

--- app/Http/Controllers/TransferController.php
+++ app/Http/Controllers/TransferController.php
@@ -174,182 +174,90 @@ class TransferController extends Controller
         ]);
     }
 
-    public function generateMockupMembers()
-    {
-        // รายการข้อมูลสมาชิก (ตัวอย่าง)
-$members = [
-    [
-        'member_id' => 'bhm516zhlqcG',
-        'username' => '0999999999',
-        'password' => '$2y$12$44.wjKRcKC3F6by9E5GFnevZSfgiMBySpNON/.UwzyaLtoVlOXDBe',
-        'wallet_balance' => '128.42',
-        'fullname' => 'กวินทรา ใช้ฮวดเจริญ',
-        'bank_name' => 'ธนาคารกสิกรไทย',
-        'bank_number' => '0999999999',
-        'bank_code' => 'bank-0',
-        'account_name' => 'กวินทรา ใช้ฮวดเจริญ',
-        'phone' => '0999999999',
-        'status_code' => 2,  // BOT.อนุมัติ
-        'level' => 'Platinum',
-    ],
-    [
-        'member_id' => 'bhm516igVqNm',
-        'username' => '0649174824',
-        'password' => '$2y$12$Gscd8Yz4Nw95R9TPGLiPQeBHjkSFH3zUW1lylNdXLrnGkOzBWR/MW',
-        'wallet_balance' => '0',
-        'fullname' => 'จักรพันธ์ มณีปกรณ์',
-        'bank_name' => 'ธนาคารกสิกรไทย',
-        'bank_number' => '0811854956',
-        'bank_code' => 'bank-0',
-        'account_name' => 'จักรพันธ์ มณีปกรณ์',
-        'phone' => '0649174824',
-        'status_code' => 1,  // BOT.ปฏิเสธ
-        'level' => 'Bronze',
-    ],
-    [
-        'member_id' => 'bhm516YvcvL4',
-        'username' => '0876543210',
-        'password' => '$2y$12$Afjjs8I1FEws9jv8tIwiMe21gxwASL8DA2W2bDe1g7We3kHS1zYpu',
-        'wallet_balance' => '1,200.00',
-        'fullname' => 'พิมพ์ชนก สุขจิตร',
-        'bank_name' => 'ธนาคารกรุงไทย',
-        'bank_number' => '0876543210',
-        'bank_code' => 'bank-1',
-        'account_name' => 'พิมพ์ชนก สุขจิตร',
-        'phone' => '0876543210',
-        'status_code' => 2,  // BOT.อนุมัติ
-        'level' => 'Silver',
-    ],
-    [
-        'member_id' => 'bhm516zfgTht',
-        'username' => '0898765432',
-        'password' => '$2y$12$96DE29nDh2X4jdsKBgDZTz11dxBoGgPxKNNkgadKnQykCjcPOg7ZO',
-        'wallet_balance' => '0',
-        'fullname' => 'นัทธมนต์ สุริยะ',
-        'bank_name' => 'ธนาคารกรุงเทพ',
-        'bank_number' => '0898765432',
-        'bank_code' => 'bank-2',
-        'account_name' => 'นัทธมนต์ สุริยะ',
-        'phone' => '0898765432',
-        'status_code' => 2,  // BOT.อนุมัติ
-        'level' => 'Gold',
-    ],
-    [
-        'member_id' => 'bhm516qdhtLz',
-        'username' => '0623456789',
-        'password' => '$2y$12$G5fyygsn04l9y9uPTd7nMzQN9K6X5x2hWiPYI1d8Aq0O0El8VR2iu',
-        'wallet_balance' => '500.50',
-        'fullname' => 'วินิจ สุรยุทธ',
-        'bank_name' => 'ธนาคารทหารไทย',
-        'bank_number' => '0623456789',
-        'bank_code' => 'bank-3',
-        'account_name' => 'วินิจ สุรยุทธ',
-        'phone' => '0623456789',
-        'status_code' => 1,  // BOT.ปฏิเสธ
-        'level' => 'Bronze',
-    ],
+  public function generateMockupMembers()
+{
+    // รายการข้อมูลสมาชิก (ตัวอย่าง)
+    $members = [
+        [
+            'member_id' => 'bhm516zhlqcG',
+            'username' => '0999999999',
+            'password' => '$2y$12$44.wjKRcKC3F6by9E5GFnevZSfgiMBySpNON/.UwzyaLtoVlOXDBe',
+            'wallet_balance' => '128.42',
+            'fullname' => 'กวินทรา ใช้ฮวดเจริญ',
+            'bank_name' => 'ธนาคารกสิกรไทย',
+            'bank_number' => '0999999999',
+            'bank_code' => 'bank-0',
+            'account_name' => 'กวินทรา ใช้ฮวดเจริญ',
+            'phone' => '0999999999',
+            'status_code' => 2,  // BOT.อนุมัติ
+            'level' => 'Platinum',
+        ],
+        [
+            'member_id' => 'bhm516igVqNm',
+            'username' => '0649174824',
+            'password' => '$2y$12$Gscd8Yz4Nw95R9TPGLiPQeBHjkSFH3zUW1lylNdXLrnGkOzBWR/MW',
+            'wallet_balance' => '0',
+            'fullname' => 'จักรพันธ์ มณีปกรณ์',
+            'bank_name' => 'ธนาคารกสิกรไทย',
+            'bank_number' => '0811854956',
+            'bank_code' => 'bank-0',
+            'account_name' => 'จักรพันธ์ มณีปกรณ์',
+            'phone' => '0649174824',
+            'status_code' => 1,  // BOT.ปฏิเสธ
+            'level' => 'Bronze',
+        ],
+        // ... (สมาชิกอื่น ๆ)
+    ];
 
-    // เพิ่มเติมสมาชิกใหม่ 2 คนที่คุณขอ
-    [
-        'member_id' => 'bhm516FQXpdG',
-        'username' => '0959988776',
-        'password' => '$2y$12$CBwFpPnLgtS0E5zvbT6Edt9csezECGBXBmfM9Jg46EFswrj4BlFCi',
-        'wallet_balance' => '745.50',
-        'fullname' => 'ไกรสร บัวหอม',
-        'bank_name' => 'ธนาคารกรุงเทพ',
-        'bank_number' => '0959988776',
-        'bank_code' => 'bank-4',
-        'account_name' => 'ไกรสร บัวหอม',
-        'phone' => '0959988776',
-        'status_code' => 2,  // BOT.อนุมัติ
-        'level' => 'Gold',
-    ],
-    [
-        'member_id' => 'bhm516XTJhUd',
-        'username' => '0847234567',
-        'password' => '$2y$12$8xzHjkdlwJkpfS8ji6cQjwPKYgb9Yr6w4KXrbdbSEjDXlsR9W.jfC',
-        'wallet_balance' => '1,150.00',
-        'fullname' => 'พิมพ์ชนก อารีย์',
-        'bank_name' => 'ธนาคารไทยพาณิชย์',
-        'bank_number' => '0847234567',
-        'bank_code' => 'bank-5',
-        'account_name' => 'พิมพ์ชนก อารีย์',
-        'phone' => '0847234567',
-        'status_code' => 2,  // BOT.อนุมัติ
-        'level' => 'Platinum',
-    ],
-    [
-        'member_id' => 'bhm516ZG3r9y',
-        'username' => '0859123456',
-        'password' => '$2y$12$k7EKcvfgyNUNyFHXmmD7fS0JJlSo01g1HXJcODdjGh8kLC4fpTlyE',
-        'wallet_balance' => '250.75',
-        'fullname' => 'สุภัทรา ศรีบุญญา',
-        'bank_name' => 'ธนาคารทหารไทย',
-        'bank_number' => '0859123456',
-        'bank_code' => 'bank-6',
-        'account_name' => 'สุภัทรา ศรีบุญญา',
-        'phone' => '0859123456',
-        'status_code' => 1,  // BOT.ปฏิเสธ
-        'level' => 'Silver',
-    ],
-    [
-        'member_id' => 'bhm516NC8cF6',
-        'username' => '0823456789',
-        'password' => '$2y$12$ovNsOSivLghsW9P57UffuORu3.9r6fn6bzNjfZPvJpycA29C7CHyS',
-        'wallet_balance' => '450.25',
-        'fullname' => 'สมปอง พรหมมา',
-        'bank_name' => 'ธนาคารกรุงไทย',
-        'bank_number' => '0823456789',
-        'bank_code' => 'bank-7',
-        'account_name' => 'สมปอง พรหมมา',
-        'phone' => '0823456789',
-        'status_code' => 2,  // BOT.อนุมัติ
-        'level' => 'Gold',
-    ]
-];
+    // Loop เพื่อ insert ข้อมูล mockup
+    foreach ($members as $member) {
+        // สุ่มวันที่จากวันที่ 1-17 มิถุนายน 2025
+        $randomDate = Carbon::create(2025, 6, rand(1, 17), rand(0, 23), rand(0, 59), rand(0, 59));
 
-
-
-        // Loop เพื่อ insert ข้อมูล mockup
-        foreach ($members as $member) {
-            DB::table('members')->insert([
-                'member_id' => $member['member_id'],
-                'username' => $member['username'],
-                'password' => $member['password'],
-                'wallet_balance' => $member['wallet_balance'],
-                'wallet_specialBuyIn' => '',
-                'wallet_lastUpdate' => '',
-                'level' => $member['level'],
-                'parent' => '',
-                'type' => '',
-                'playId' => '',
-                'currency' => '',
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-                'fullname' => $member['fullname'],
-                'bank_name' => $member['bank_name'],
-                'bank_number' => $member['bank_number'],
-                'bank_code' => $member['bank_code'],
-                'birth_date' => NULL,
-                'account_name' => $member['account_name'],
-                'phone' => $member['phone'],
-                'enable' => 1,
-                'active' => 1,
-                'update_by' => 0,
-                'ref_click_link' => 0,
-                'ref_user' => '',
-                'ref_commission' => 0.00,
-                'token' => '',
-                'ranking' => '',
-                'source' => '',
-                'role' => '',
-                'nickname' => '',
-                'remaining_spin' => 0.00,
-            ]);
+        // ถ้าเวลาที่สุ่มเกินเวลาปัจจุบัน ให้ปรับเวลาปัจจุบัน
+        if ($randomDate->greaterThan(Carbon::now())) {
+            $randomDate = Carbon::now();
         }
 
-        // return response()->json([
-        //     'message' => 'Generated Mockup Member Data successfully!',
-        // ]);
+        DB::table('members')->insert([
+            'member_id' => $member['member_id'],
+            'username' => $member['username'],
+            'password' => $member['password'],
+            'wallet_balance' => $member['wallet_balance'],
+            'wallet_specialBuyIn' => '',
+            'wallet_lastUpdate' => '',
+            'level' => $member['level'],
+            'parent' => '',
+            'type' => '',
+            'playId' => '',
+            'currency' => '',
+            'created_at' => $randomDate,
+            'updated_at' => $randomDate,
+            'fullname' => $member['fullname'],
+            'bank_name' => $member['bank_name'],
+            'bank_number' => $member['bank_number'],
+            'bank_code' => $member['bank_code'],
+            'birth_date' => NULL,
+            'account_name' => $member['account_name'],
+            'phone' => $member['phone'],
+            'enable' => 1,
+            'active' => 1,
+            'update_by' => 0,
+            'ref_click_link' => 0,
+            'ref_user' => '',
+            'ref_commission' => 0.00,
+            'token' => '',
+            'ranking' => '',
+            'source' => '',
+            'role' => '',
+            'nickname' => '',
+            'remaining_spin' => 0.00,
+        ]);
     }
+
+    return response()->json([
+        'message' => 'Generated Mockup Member Data successfully!',
+    ]);
+}
+
 }
