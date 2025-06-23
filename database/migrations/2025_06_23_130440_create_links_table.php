d084d4e59edd7b9869a45e4846c5d8376a87afd0
alwdev <69068330+alwdev@users.noreply.github.com>
2025-06-23 16:19:15 +0700

    Update 2025_06_23_130440_create_links_table.php

--- database/migrations/2025_06_23_130440_create_links_table.php
+++ database/migrations/2025_06_23_130440_create_links_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('links', function (Blueprint $table) {
             $table->id();
             $table->string('link');
-            $table->json('hashtags')->nullable(); // เก็บ Hashtags เป็น JSON array
+            $table->text('hashtags')->nullable(); 
             $table->timestamps();
         });
     }
