9218393d19d4aba617c802d891c448260de2120a
alwdev <69068330+alwdev@users.noreply.github.com>
2025-07-01 18:49:35 +0700

    document-th

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -86,7 +86,20 @@
                     {{-- @endif --}}
                 @endif
                 <li>
-                    <a href="document.pdf" target="_blank" class="waves-effect"><i class='bx bx-copy'></i><span>{{__('main.user_manual')}}</span></a>
+                    
+
+                    @if (session()->has('locale'))
+                        @if(session()->get('locale') == 'th')
+                            <a href="document-th.pdf" target="_blank" class="waves-effect"><i class='bx bx-copy'></i><span>{{__('main.user_manual')}}</span></a>
+                        @else
+                            <a href="document.pdf" target="_blank" class="waves-effect"><i class='bx bx-copy'></i><span>{{__('main.user_manual')}}</span></a>
+                        @endif
+                    @else
+                        @php
+                            Session::put('locale', 'en');
+                        @endphp
+                        <a href="document.pdf" target="_blank" class="waves-effect"><i class='bx bx-copy'></i><span>{{__('main.user_manual')}}</span></a>
+                    @endif
                 </li>
             </ul>
         </div>
