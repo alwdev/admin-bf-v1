dba6d675ecabc53a8da18f6d7286a2774fde8fbc
alwdev <69068330+alwdev@users.noreply.github.com>
2025-07-14 18:03:53 +0700

    + 20%

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -188,7 +188,7 @@ class TransactionController extends Controller
                     ->line("amount = " . $request->amount)
                     ->line("acc_no = " . $request->acc_no)
                     ->send();
-                    return 404;
+                return 404;
             }
 
             return 404;
@@ -236,8 +236,17 @@ class TransactionController extends Controller
                         } else {
                             error_log("Not meeting new member conditions");
                             $message .= "Not meeting new member conditions, ";
-                            $member->wallet_balance =  (float) $member->wallet_balance + $transfer->amount;
-                            $amount_betflix = $transfer->amount;
+                            if ($member->created_at && $member->created_at->gt(Carbon::now()->subDays(7))) {
+                                // ผู้ใช้ถูกสร้างภายใน 7 วันที่ผ่านมา
+                                error_log("");
+                                $bonus = $transfer->amount*0.2;  //+ 20%
+                                $member->wallet_balance =  (float) $member->wallet_balance + $transfer->amount + $bonus;
+                                $amount_betflix = $transfer->amount + $bonus;
+                                $transfer->promotion = $pro->name;
+                            }else{
+                                $member->wallet_balance =  (float) $member->wallet_balance + $transfer->amount;
+                                $amount_betflix = $transfer->amount;
+                            }
                         }
                     } else { //โปร member ทุกคน
                         error_log("Promo member new");
