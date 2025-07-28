5b2379550cbb96683a447123ed518c3abcafa1b4
alwdev <69068330+alwdev@users.noreply.github.com>
2025-07-28 13:07:19 +0700

    Update ManageMemberController.php

--- app/Http/Controllers/ManageMemberController.php
+++ app/Http/Controllers/ManageMemberController.php
@@ -278,7 +278,7 @@ class ManageMemberController extends Controller
                     $transfer->promotion = 'ไม่มีโปรโมชั่น'; // หรือค่า default อื่นๆ
                     // $transfer->required_turnover_amount = 0.0; // ไม่มีโปรโมชั่นก็ไม่มีเทิร์น
                     // $transfer->bonus_applied = 0.0;
-                    $transfer->promotion_id = null; // ไม่มีโปรโมชั่นก็เป็น null
+                    $transfer->promotion_id = 0; // ไม่มีโปรโมชั่นก็เป็น null
                 }
 
                 error_log('Bonus = ' . $bonus); // ตัวแปร $bonus นี้จะถูกใช้ใน Telegram
