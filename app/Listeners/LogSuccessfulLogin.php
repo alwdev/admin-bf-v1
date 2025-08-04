ca39b154acc8207ab3ce5d7a901562cc2ac3f598
alwdev <69068330+alwdev@users.noreply.github.com>
2025-08-04 13:53:57 +0700

    login

---
+++ app/Listeners/LogSuccessfulLogin.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Login;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use App\Models\LoginLog; // เพิ่ม Model ที่เราสร้าง
+
+class LogSuccessfulLogin
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(Login $event): void
+    {
+        LoginLog::create([
+            'user_id' => $event->user->id,
+            'ip_address' => request()->ip(),
+            'user_agent' => request()->userAgent(),
+            'login_at' => now(),
+        ]);
+    }
+}
