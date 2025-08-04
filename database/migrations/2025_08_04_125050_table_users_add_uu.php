a8b8ef127c862c40de2be36ef6eec44dc3665c46
alwdev <69068330+alwdev@users.noreply.github.com>
2025-08-04 12:56:03 +0700

    Update 2025_08_04_125050_table_users_add_uu.php

--- database/migrations/2025_08_04_125050_table_users_add_uu.php
+++ database/migrations/2025_08_04_125050_table_users_add_uu.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             //
-            $table->string('uu')->nullable()->after('truepass');
+            $table->string('uu')->nullable();
         });
     }
 
