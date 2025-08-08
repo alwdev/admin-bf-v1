eb67f7a881c5865d67333cbcba99a9fd0259cbb9
alwdev <69068330+alwdev@users.noreply.github.com>
2025-08-08 12:22:15 +0700

    json_encode

--- app/Http/Controllers/AppWalletController.php
+++ app/Http/Controllers/AppWalletController.php
@@ -65,7 +65,7 @@ class AppWalletController extends Controller
         $payload = $request->getContent();
         $data = json_decode($payload, true);
         Logs::create([
-            'log' =>'moonpay'. $request->getContent()
+            'log' =>'moonpay'. json_encode($request->getContent())
         ]);
         Log::info('MoonPay Webhook:', $data);
 
