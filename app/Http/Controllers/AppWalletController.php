e832d15bcc6beadbc98d95b2dcfdd23463bd7d9b
alwdev <69068330+alwdev@users.noreply.github.com>
2025-08-08 12:33:36 +0700

    ['type']

--- app/Http/Controllers/AppWalletController.php
+++ app/Http/Controllers/AppWalletController.php
@@ -67,7 +67,7 @@ class AppWalletController extends Controller
         Logs::create([
             'log' =>'moonpay'. json_encode($data)
         ]);
-
+        Log::info('MoonPay type:', $data['type']);
 
         if ($data['type'] === 'transaction_updated' && $data['data']['status'] === 'completed') {
             $tx = $data['data'];
