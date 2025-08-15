4b2850c7499d9a80c26cdfbfe6ce3f52aa9df905
alwdev <69068330+alwdev@users.noreply.github.com>
2025-08-15 20:08:25 +0700

    bank

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -415,8 +415,8 @@ public function lineNotify_deposit($id)
             if ($bank) {
                 // หากคุณเคยเพิ่ม $transfer->amount เข้า bank->balance ในบล็อก success
                 // ก็ควรหักออกในบล็อก failure นี้
-                // $bank->balance = (float) $bank->balance + (float) $transfer->amount;
-                // $bank->save();
+                $bank->balance = (float) $bank->balance + (float) $transfer->amount;
+                $bank->save();
             }
 
             // บันทึก PromotionUsed ก็ต่อเมื่อมีการใช้โปรโมชั่นจริง
