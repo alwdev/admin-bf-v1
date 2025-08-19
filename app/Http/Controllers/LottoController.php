cbdb8bb87796c047b372271269dd4f820c7ae47b
alwdev <69068330+alwdev@users.noreply.github.com>
2025-08-19 10:35:16 +0700

    get_balance log

--- app/Http/Controllers/LottoController.php
+++ app/Http/Controllers/LottoController.php
@@ -9,7 +9,13 @@ class LottoController extends Controller
 {
     public function get_balance(Request $request)
     {
+        $log = new \App\Models\Logs;
+        $log->log = "Lotto Get Balance updated for {$request->username}";
+        $log->save();
         $member = Members::where('username', $request->username)->first();
+        if (!$member) {
+            return response()->json(['status' => 'error', 'message' => 'Member not found.'], 404);
+        }           
         return $member->wallet_balance;
     }
     // Ubdate Balance
@@ -22,7 +28,7 @@ class LottoController extends Controller
             $member->save();
 
             $bf_deposit = app(\App\Http\Controllers\BetflixController::class)->Master_Withdraw($member->username, ($request->balance));
-            if($bf_deposit == 'success') {
+            if ($bf_deposit == 'success') {
                 // Log the deposit transaction
                 $log = new \App\Models\Logs;
                 $log->log = "Lotto Balance updated for {$member->username} with amount {$request->balance}";
