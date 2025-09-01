9a55a84e2127bc96e3612b087fab2b473ab60c6f
alwdev <69068330+alwdev@users.noreply.github.com>
2025-09-01 15:24:41 +0700

    floor

--- app/Http/Controllers/ManageMemberController.php
+++ app/Http/Controllers/ManageMemberController.php
@@ -1009,7 +1009,7 @@ class ManageMemberController extends Controller
             $member->save();
 
             // เรียก API เพื่อฝากเงินเข้า Betflix
-            $bf_deposit = app(BetflixController::class)->Master_Deposit($member->username, $commission);
+            $bf_deposit = app(BetflixController::class)->Master_Deposit($member->username,floor($commission));
             Log::info('Betflix commission ' . $bf_deposit . ' ' . $commission . ' User = ' . $member->username);
 
             if ($bf_deposit === "success") {
