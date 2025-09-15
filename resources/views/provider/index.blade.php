97f625e15811955eeff9493c56a888c45bc5999d
alwdev <69068330+alwdev@users.noreply.github.com>
2025-09-15 17:49:57 +0700

    icon

--- resources/views/provider/index.blade.php
+++ resources/views/provider/index.blade.php
@@ -394,7 +394,7 @@
 
             Swal.fire({
                 position: 'top-end',
-                type: 'success',
+                icon: 'success',
                 title: 'Your work has been saved',
                 showConfirmButton: false,
                 timer: 1500
@@ -404,7 +404,7 @@
 
             Swal.fire({
                 position: 'top-end',
-                type: 'success',
+                icon: 'success',
                 title: 'Your work has been saved',
                 showConfirmButton: false,
                 timer: 1500
