d6ec2e017ec729d47ff05ac0e641981c2390ba26
alwdev <69068330+alwdev@users.noreply.github.com>
2025-09-24 17:42:34 +0700

    value

--- resources/views/setting/alert_index.blade.php
+++ resources/views/setting/alert_index.blade.php
@@ -202,8 +202,7 @@
                 denyButtonText: '{{ __('main.cancel') }}'
             }).then((result) => {
                 /* Read more about isConfirmed, isDenied below */
-                console.log(result);
-                if (result.isConfirmed) {
+                if (result.value) {
                     $('#destroy_id').val(id);
                     $('#destroy_form').submit();
                 } else {}
