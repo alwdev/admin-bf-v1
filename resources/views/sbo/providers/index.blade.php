ceace756b07d9a747c33814f694d7e3988176f75
alwdev <69068330+alwdev@users.noreply.github.com>
2026-03-10 18:05:16 +0700

    alert

--- resources/views/sbo/providers/index.blade.php
+++ resources/views/sbo/providers/index.blade.php
@@ -17,6 +17,14 @@
     <div class="row">
         <div class="col-12 card">
             <div class="card-body">
+                @if (session('success'))
+                    <div class="alert alert-success alert-dismissible fade show" role="alert">
+                        {{ session('success') }}
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                @endif
                 @if (session('error'))
                     <div class="alert alert-danger alert-dismissible fade show" role="alert">
                         {{ session('error') }}
